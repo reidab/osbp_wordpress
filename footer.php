efb72adcb1519cc481e258512cc1c47754206926
Reid Beels <mail@reidbeels.com>
2009-03-11 15:08:10 -0700

    Adding clicky analytics

--- footer.php
+++ footer.php
@@ -6,5 +6,8 @@
 
 <?php wp_footer() ?>
 
+<script src="http://static.getclicky.com/79611.js" type="text/javascript"></script>
+<noscript><p><img alt="Clicky" width="1" height="1" src="http://static.getclicky.com/79611-db10.gif" /></p></noscript>
+
 </body>
 </html>
