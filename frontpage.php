e2c293be66ca0b4c6517cab546eaca43ab43c5a8
Reid Beels <mail@reidbeels.com>
2009-03-24 03:29:43 -0700

    removing extra closing tag

--- frontpage.php
+++ frontpage.php
@@ -42,75 +42,41 @@ Template Name: Front Page
             </div>
          </div>
          <div id="frontpage_content">
-            <div class='frontpage_snippet' id='citizen_snippet'>
-               <h4>Open Source Citizenship</h4>
-                  <p>What are the rights and responsibilities of an open source citizen?
-                  We're exploring what open source means to us, what it offers, 
-                  where we struggle, and why we do this day in and day out, even when 
-                  we’re not paid for it.</p>
-            </div>
-            
-            <div class='frontpage_snippet' id='tracks_snippet'>
-               <h4>Innovative Track Structure</h4>
-                  <p>Our session tracks are technology agnostic, based around shared 
-                  community experiences and focus on the similarities between 
-                  projects, not the differences. 
-                  <a href="/events/2009/tracks">View the tracks.</a></p>
-            </div>
-            
-            <div class='frontpage_snippet' id='lounge_snippet'>
-               <h4>All-Hours Hacker Lounge</h4>
-                  <p>The geekery doesn’t end when the sessions do. There will also be 
-                  a 24-hour hacker lounge for code sprints, bug bashes, session deep 
-                  dives, bouncing ideas, starting new projects or just mingling and 
-                  taking in the vibe.</p>
-            </div>
-                  
-            <div class='frontpage_snippet' id='volunteer_snippet'>
-               <h4>100% Volunteer-Run</h4>
-                  <p>Your software is peer-produced. Why not your conference? 
-                  Open Source Bridge is pioneered and planned by a team of 
-                  open source developers and technologists. What's more, we're
-                  <a href='http://github.com/igal/openconferenceware'>building an
-                  open source application</a> to manage talk proposals.</p>
-            </div>
+            <?php the_content(); ?>
          </div>
       </div>
+   </div>
+   <div class='inner_container'>
+      <div id="frontpage_blog">
+         <h2 id='latest_news_head'>Latest News</h2>
+            
+         <?php query_posts('showposts=1'); ?>
+            
+      <?php while ( have_posts() ) : the_post() ?>
+
+         <div id="post-<?php the_ID() ?>" class="<?php sandbox_post_class() ?>">
+            <h2 class="entry-title"><a href="<?php the_permalink() ?>" title="<?php printf( __('Permalink to %s', 'sandbox'), the_title_attribute('echo=0') ) ?>" rel="bookmark"><?php the_title() ?></a></h2>
+            <div class="entry-date"><abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO') ?>"><?php unset($previousday); printf( __( '%1$s &#8211; %2$s', 'sandbox' ), the_date( '', '', '', false ), get_the_time() ) ?></abbr></div>
+            <div class="entry-content">
+               <?php the_content( __( 'Read More <span class="meta-nav">&raquo;</span>', 'sandbox' ) ) ?>
+               <?php wp_link_pages('before=<div class="page-link">' . __( 'Pages:', 'sandbox' ) . '&after=</div>') ?>
+            </div>
+            <div class="entry-meta">
+               <span class="author vcard"><?php printf( __( 'By %s', 'sandbox' ), '<a class="url fn n" href="' . get_author_link( false, $authordata->ID, $authordata->user_nicename ) . '" title="' . sprintf( __( 'View all posts by %s', 'sandbox' ), $authordata->display_name ) . '">' . get_the_author() . '</a>' ) ?></span>
+               <span class="meta-sep">|</span>
+               <span class="cat-links"><?php printf( __( 'Posted in %s', 'sandbox' ), get_the_category_list(', ') ) ?></span>
+               <span class="meta-sep">|</span>
+               <?php the_tags( __( '<span class="tag-links">Tagged ', 'sandbox' ), ", ", "</span>\n\t\t\t\t\t<span class=\"meta-sep\">|</span>\n" ) ?>
+               <?php edit_post_link( __( 'Edit', 'sandbox' ), "\t\t\t\t\t<span class=\"edit-link\">", "</span>\n\t\t\t\t\t<span class=\"meta-sep\">|</span>\n" ) ?>
+               <span class="comments-link"><?php comments_popup_link( __( 'Comments (0)', 'sandbox' ), __( 'Comments (1)', 'sandbox' ), __( 'Comments (%)', 'sandbox' ) ) ?></span>
+            </div>
+         </div><!-- .post -->
+
+         <?php comments_template() ?>
+
+         <?php endwhile; ?>
+            
       </div>
-      <div class='inner_container'>
-         <div id="frontpage_blog">
-            <h2 id='latest_news_head'>Latest News</h2>
-            
-            <?php query_posts('showposts=1'); ?>
-            
-            <?php while ( have_posts() ) : the_post() ?>
-
-            			<div id="post-<?php the_ID() ?>" class="<?php sandbox_post_class() ?>">
-            				<h2 class="entry-title"><a href="<?php the_permalink() ?>" title="<?php printf( __('Permalink to %s', 'sandbox'), the_title_attribute('echo=0') ) ?>" rel="bookmark"><?php the_title() ?></a></h2>
-            				<div class="entry-date"><abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO') ?>"><?php unset($previousday); printf( __( '%1$s &#8211; %2$s', 'sandbox' ), the_date( '', '', '', false ), get_the_time() ) ?></abbr></div>
-            				<div class="entry-content">
-            <?php the_content( __( 'Read More <span class="meta-nav">&raquo;</span>', 'sandbox' ) ) ?>
-
-            				<?php wp_link_pages('before=<div class="page-link">' . __( 'Pages:', 'sandbox' ) . '&after=</div>') ?>
-            				</div>
-            				<div class="entry-meta">
-            					<span class="author vcard"><?php printf( __( 'By %s', 'sandbox' ), '<a class="url fn n" href="' . get_author_link( false, $authordata->ID, $authordata->user_nicename ) . '" title="' . sprintf( __( 'View all posts by %s', 'sandbox' ), $authordata->display_name ) . '">' . get_the_author() . '</a>' ) ?></span>
-            					<span class="meta-sep">|</span>
-            					<span class="cat-links"><?php printf( __( 'Posted in %s', 'sandbox' ), get_the_category_list(', ') ) ?></span>
-            					<span class="meta-sep">|</span>
-            					<?php the_tags( __( '<span class="tag-links">Tagged ', 'sandbox' ), ", ", "</span>\n\t\t\t\t\t<span class=\"meta-sep\">|</span>\n" ) ?>
-            <?php edit_post_link( __( 'Edit', 'sandbox' ), "\t\t\t\t\t<span class=\"edit-link\">", "</span>\n\t\t\t\t\t<span class=\"meta-sep\">|</span>\n" ) ?>
-            					<span class="comments-link"><?php comments_popup_link( __( 'Comments (0)', 'sandbox' ), __( 'Comments (1)', 'sandbox' ), __( 'Comments (%)', 'sandbox' ) ) ?></span>
-            				</div>
-            			</div><!-- .post -->
-
-            <?php comments_template() ?>
-
-            <?php endwhile; ?>
-            
-         </div>
-      </div>
-
-   </div><!-- #container -->
+   </div>
 
 <?php get_footer() ?>
