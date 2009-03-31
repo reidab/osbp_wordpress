e30ca19c2c4902f1a72c23d86f3fe7c9ef1ca4b0
Reid Beels <mail@reidbeels.com>
2009-03-31 13:20:01 -0700

    Updating deadline to April 10.

--- frontpage.php
+++ frontpage.php
@@ -34,11 +34,11 @@ Template Name: Front Page
          </div>
          <div id="frontpage_callouts">
             <div class='register-callout'>
-               Registration is only <strong>$175</strong> until <strong>March 31</strong>.
+               Registration is only <strong>$175</strong> until <strong>April 10</strong>.
                <a href='/attend' class='register-button'>Register Today</a>
             </div>
             <div class='submit-callout'>
-               Proposals are due <strong>March 31</strong>.
+               Proposals are due <strong>April 10</strong>.
                <a href='/call-for-proposals' class='propose-button'>Submit Yours Now</a>
             </div>
          </div>
--- header.php
+++ header.php
@@ -23,7 +23,7 @@
    		   <h2>The conference for open source citizens</h2>
    		   <p id='conference-date-location'>June 17&ndash;19, 2009 <span class='separator'>|</span> Portland, Oregon</p>
 		   </div>
-		   <a href='/attend' title='Early bird discount ends March 31st. Register today!' id="header-reg-now">Register now and save!</a>
+		   <a href='/attend' title='Early bird discount ends April 10th. Register today!' id="header-reg-now">Register now and save!</a>
 		</div>
 	</div><!--  #header -->
 
