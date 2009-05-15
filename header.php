1245c1e5bdeb47cf35376643320e3f4de54adb02
Reid Beels <mail@reidbeels.com>
2009-05-14 23:14:36 -0700

    Adding main nav item for attendee wiki

--- header.php
+++ header.php
@@ -20,34 +20,34 @@
 
 -->
 <head profile="http://gmpg.org/xfn/11">
-	<title><?php wp_title( '/', true, 'right' ); ?>Open Source Bridge: The conference for open source citizens / June 17-19, 2009 / Portland, OR</title>
-	<meta http-equiv="content-type" content="<?php bloginfo('html_type') ?>; charset=<?php bloginfo('charset') ?>" />
-	<link rel="stylesheet" type="text/css" href="http://opensourcebridge.org/common/osbp_common_v3.css" />
-	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url') ?>" />
+   <title><?php wp_title( '/', true, 'right' ); ?>Open Source Bridge: The conference for open source citizens / June 17-19, 2009 / Portland, OR</title>
+   <meta http-equiv="content-type" content="<?php bloginfo('html_type') ?>; charset=<?php bloginfo('charset') ?>" />
+   <link rel="stylesheet" type="text/css" href="http://opensourcebridge.org/common/osbp_common_v3.css" />
+   <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url') ?>" />
 <?php wp_head() // For plugins ?>
-	<link rel="alternate" type="application/rss+xml" href="<?php bloginfo('rss2_url') ?>" title="<?php printf( __( '%s latest posts', 'sandbox' ), wp_specialchars( get_bloginfo('name'), 1 ) ) ?>" />
-	<link rel="alternate" type="application/rss+xml" href="<?php bloginfo('comments_rss2_url') ?>" title="<?php printf( __( '%s latest comments', 'sandbox' ), wp_specialchars( get_bloginfo('name'), 1 ) ) ?>" />
-	<link rel="alternate" type="application/atom+xml" href="/proposals.atom" title="Open Source Bridge Proposal Submissions" />
-	<link rel="pingback" href="<?php bloginfo('pingback_url') ?>" />
+   <link rel="alternate" type="application/rss+xml" href="<?php bloginfo('rss2_url') ?>" title="<?php printf( __( '%s latest posts', 'sandbox' ), wp_specialchars( get_bloginfo('name'), 1 ) ) ?>" />
+   <link rel="alternate" type="application/rss+xml" href="<?php bloginfo('comments_rss2_url') ?>" title="<?php printf( __( '%s latest comments', 'sandbox' ), wp_specialchars( get_bloginfo('name'), 1 ) ) ?>" />
+   <link rel="alternate" type="application/atom+xml" href="/proposals.atom" title="Open Source Bridge Proposal Submissions" />
+   <link rel="pingback" href="<?php bloginfo('pingback_url') ?>" />
 </head>
 
 <body class="<?php sandbox_body_class() ?>">
 
 <div id="wrapper" class="hfeed">
 
-	<div id="header">
-	   <div class='inner_container'>
-   		<h1 id="site-title"><span><a href="<?php bloginfo('home') ?>/" title="<?php echo wp_specialchars( get_bloginfo('name'), 1 ) ?>" rel="home"><?php bloginfo('name') ?></a></span></h1>
-   		<div id="site-description">
-   		   <h2>The conference for open source citizens</h2>
-   		   <p id='conference-date-location'>June 17&ndash;19, 2009 <span class='separator'>|</span> Portland, Oregon</p>
-		   </div>
-		   <a href='/attend' title='Register today!' id="header-reg-now">Register today!</a>
-		</div>
-	</div><!--  #header -->
+   <div id="header">
+      <div class='inner_container'>
+         <h1 id="site-title"><span><a href="<?php bloginfo('home') ?>/" title="<?php echo wp_specialchars( get_bloginfo('name'), 1 ) ?>" rel="home"><?php bloginfo('name') ?></a></span></h1>
+         <div id="site-description">
+            <h2>The conference for open source citizens</h2>
+            <p id='conference-date-location'>June 17&ndash;19, 2009 <span class='separator'>|</span> Portland, Oregon</p>
+         </div>
+         <a href='/attend' title='Register today!' id="header-reg-now">Register today!</a>
+      </div>
+   </div><!--  #header -->
 
    <?php
-   
+
    global $wp_query;
 
    if( empty($wp_query->post->post_parent) ) {
@@ -55,38 +55,39 @@
    } else {
       $parent = $wp_query->post->post_parent;
    }
-   
+
    ?>
 
 
-	<div id="access">
-		<div class="skip-link"><a href="#content" title="<?php _e( 'Skip to content', 'sandbox' ) ?>"><?php _e( 'Skip to content', 'sandbox' ) ?></a></div>
+   <div id="access">
+      <div class="skip-link"><a href="#content" title="<?php _e( 'Skip to content', 'sandbox' ) ?>"><?php _e( 'Skip to content', 'sandbox' ) ?></a></div>
 
-		<div id="menu">
-			<ul>
-			   <li id="menu_first_item" <?php print ($parent==10 ? 'class="current_page_item"' : ''); ?>><a href='/about/' title='About' class=''>About</a></li>
-				<li <?php print ($parent==211 ? 'class="current_page_item"' : ''); ?>><a href='/attend/' title='Attend' class=''>Attend</a></li>
-				<li class="page_item"><a href='/sessions/' title='Sessions' class=''>Sessions</a></li>
-				<li <?php print ($parent==56 ? 'class="current_page_item"' : ''); ?>><a href="/volunteer/" title="Volunteer">Get Involved</a></li>
-				<li <?php print ($parent==173 ? 'class="current_page_item"' : ''); ?>><a href="/sponsors/" title="Sponsors">Sponsors</a></li>
-				<li <?php print (!is_page() ? 'class="current_page_item"' : ''); ?>><a href="/blog">Blog</a></li>
-			</ul>
+      <div id="menu">
+         <ul>
+            <li id="menu_first_item" <?php print ($parent==10 ? 'class="current_page_item"' : ''); ?>><a href='/about/' title='About' class=''>About</a></li>
+            <li <?php print ($parent==211 ? 'class="current_page_item"' : ''); ?>><a href='/attend/' title='Attend' class=''>Attend</a></li>
+            <li class="page_item"><a href='/sessions/' title='Sessions' class=''>Sessions</a></li>
+            <li <?php print ($parent==56 ? 'class="current_page_item"' : ''); ?>><a href="/volunteer/" title="Volunteer">Get Involved</a></li>
+            <li <?php print ($parent==173 ? 'class="current_page_item"' : ''); ?>><a href="/sponsors/" title="Sponsors">Sponsors</a></li>
+            <li <?php print (!is_page() ? 'class="current_page_item"' : ''); ?>><a href="/blog">Blog</a></li>
+            <li class="page_item"><a href='/wiki/' title='Wiki' class=''>Wiki</a></li>
+         </ul>
       </div>
-      
+
       <?php
-      if (!is_front_page()) { 
-      
+      if (!is_front_page()) {
+
       if(is_page()) {
          $section_title = get_the_title($parent);
       } else {
          $section_title = "Blog";
       }
       ?>
-      
+
       <div id="subnav" class='navbar'>
          <div class='inner_container'>
          <h2><?php echo $section_title ?></h2>
-      
+
       <?php if(wp_list_pages("title_li=&child_of=$parent&echo=0" )): ?>
          <ul>
             <li class="page_item<?php echo ($wp_query->post->ID == $parent) ? ' current_page_item' : '' ?>">
@@ -98,4 +99,4 @@
          </div>
       </div>
       <?php } ?>
-	</div><!-- #access -->
+   </div><!-- #access -->
