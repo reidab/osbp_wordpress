e5a2974ecfe033329028a9f03ac5726ae833d246
Igal Koshevoy <igal@pragmaticraft.com>
2010-02-14 06:00:16 -0800

    Updated header.php to use shared fragments.

--- header.php
+++ header.php
@@ -36,46 +36,21 @@
 <body class="<?php sandbox_body_class() ?>">
 
 <div id="wrapper" class="hfeed">
-
-   <div id="header">
-      <div class='inner_container'>
-         <h1 id="site-title"><span><a href="<?php bloginfo('home') ?>/" title="<?php echo wp_specialchars( get_bloginfo('name'), 1 ) ?>" rel="home"><?php bloginfo('name') ?></a></span></h1>
-         <div id="site-description">
-            <h2>The conference for open source citizens</h2>
-            <p id='conference-date-location'>June 1&ndash;4, 2010 <span class='separator'>|</span> Portland, Oregon</p>
-         </div>
-      </div>
-   </div><!--  #header -->
-
    <?php
-
-   global $wp_query;
-
-   if( empty($wp_query->post->post_parent) ) {
-      $parent = $wp_query->post->ID;
-   } else {
-      $parent = $wp_query->post->post_parent;
-   }
-
+     global $wp_query;
+     if( empty($wp_query->post->post_parent) ) {
+        $parent = $wp_query->post->ID;
+     } else {
+        $parent = $wp_query->post->post_parent;
+     }
    ?>
 
+    <!-- #header_fragment BEGIN -->
+    <?php require_once 'shared_fragments.php'; ?>
+<?php echo_shared_fragment('header_current'); ?>
+    <!-- #header_fragment END -->
 
-   <div id="access">
-      <div class="skip-link"><a href="#content" title="<?php _e( 'Skip to content', 'sandbox' ) ?>"><?php _e( 'Skip to content', 'sandbox' ) ?></a></div>
-
-      <div id="menu">
-         <ul>
-            <li id="menu_first_item" <?php print ($parent==10 ? 'class="current_page_item"' : ''); ?>><a href='/about/' title='About' class=''>About</a></li>
-            <li <?php print ($parent==211 ? 'class="current_page_item"' : ''); ?>><a href='/attend/' title='Attend' class=''>Attend</a></li>
-            <li class="page_item"><a href='/sessions/' title='Sessions' class=''>Sessions</a></li>
-            <li <?php print ($parent==56 ? 'class="current_page_item"' : ''); ?>><a href="/volunteer/" title="Volunteer">Get Involved</a></li>
-            <li <?php print ($parent==173 ? 'class="current_page_item"' : ''); ?>><a href="/sponsors/" title="Sponsors">Sponsors</a></li>
-            <li <?php print (!is_page() ? 'class="current_page_item"' : ''); ?>><a href="/blog">Blog</a></li>
-            <li class="page_item"><a href='/wiki/' title='Wiki' class=''>Wiki</a></li>
-         </ul>
-      </div>
-
-      <?php
+    <?php
       if (!is_front_page()) {
 
       if(is_page()) {
@@ -83,21 +58,21 @@
       } else {
          $section_title = "Blog";
       }
-      ?>
+    ?>
 
-      <div id="subnav" class='navbar'>
-         <div class='inner_container'>
-         <h2><?php echo $section_title ?></h2>
+    <div id="subnav" class='navbar'>
+       <div class='inner_container'>
+       <h2><?php echo $section_title ?></h2>
 
-      <?php if($parent && wp_list_pages("title_li=&child_of=$parent&echo=0" )): ?>
-         <ul>
-            <li class="page_item<?php echo ($wp_query->post->ID == $parent) ? ' current_page_item' : '' ?>">
-               <a href="<?php echo get_permalink($parent) ?>" title="<?php echo $section_title ?>">Overview</a>
-            </li>
-            <?php wp_list_pages("title_li=&child_of=$parent" ); ?>
-         </ul>
-      <?php endif; ?>
-         </div>
-      </div>
-      <?php } ?>
-   </div><!-- #access -->
+    <?php if($parent && wp_list_pages("title_li=&child_of=$parent&echo=0" )): ?>
+       <ul>
+          <li class="page_item<?php echo ($wp_query->post->ID == $parent) ? ' current_page_item' : '' ?>">
+             <a href="<?php echo get_permalink($parent) ?>" title="<?php echo $section_title ?>">Overview</a>
+          </li>
+          <?php wp_list_pages("title_li=&child_of=$parent" ); ?>
+       </ul>
+    <?php endif; ?>
+       </div>
+    </div>
+    <?php } ?>
+<!-- #header -->
