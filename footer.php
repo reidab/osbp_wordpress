ccb926a528d8e74fd6cc62b1b2b65410e234c927
Igal Koshevoy <igal@pragmaticraft.com>
2010-02-17 23:35:19 -0800

    Improved footer: deferred loading of 'getclicky' because it's external and slow.

--- footer.php
+++ footer.php
@@ -7,7 +7,15 @@
 
 <?php wp_footer() ?>
 
-<script src="http://static.getclicky.com/79611.js" type="text/javascript"></script>
+<script type="text/javascript">
+    // Defer loading "getclicky" till after the page has loaded, because it's external and slow.
+    $(document).ready(function () {
+        var element = document.createElement("script");
+        element.src = "http://static.getclicky.com/79611.js";
+        element.type = "text/javascript";
+        document.getElementsByTagName("head")[0].appendChild(element);
+    });
+</script>
 <noscript><p><img alt="Clicky" width="1" height="1" src="http://static.getclicky.com/79611-db10.gif" /></p></noscript>
 
 </body>
