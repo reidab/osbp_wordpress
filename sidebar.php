d4cc86f3f3859947e9e43e9a133b205e28b2b2f9
Igal Koshevoy <igal@pragmaticraft.com>
2010-02-18 17:52:58 -0800

    Refactored sidebar's whitespace -- no changes to logic.

--- sidebar.php
+++ sidebar.php
@@ -1,76 +1,74 @@
-	<div id="primary" class="sidebar">
-		<ul class="xoxo">
-		  <?php if($parent && wp_list_pages("title_li=&child_of=$parent&echo=0" )): ?>
-		    <li id='subnav'>
-           <ul>
-              <li class="page_item<?php echo ($wp_query->post->ID == $parent) ? ' current_page_item' : '' ?>">
-                 <a href="<?php echo get_permalink($parent) ?>" title="<?php echo $section_title ?>">Overview</a>
-              </li>
-              <?php wp_list_pages("title_li=&child_of=$parent" ); ?>
-           </ul>
-         </li>
-      <?php endif; ?>
-<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar(1) ) : // begin primary sidebar widgets ?>
+<div id="primary" class="sidebar">
+  <ul class="xoxo">
+    <?php if($parent && wp_list_pages("title_li=&child_of=$parent&echo=0" )): ?>
+      <li id='subnav'>
+        <ul>
+            <li class="page_item<?php echo ($wp_query->post->ID == $parent) ? ' current_page_item' : '' ?>">
+              <a href="<?php echo get_permalink($parent) ?>" title="<?php echo $section_title ?>">Overview</a>
+            </li>
+            <?php wp_list_pages("title_li=&child_of=$parent" ); ?>
+        </ul>
+      </li>
+    <?php endif; ?>
 
-			<li id="pages">
-				<h3><?php _e( 'Pages', 'sandbox' ) ?></h3>
-				<ul>
-<?php wp_list_pages('title_li=&sort_column=menu_order' ) ?>
-				</ul>
-			</li>
+    <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar(1) ) : // begin primary sidebar widgets ?>
+      <li id="pages">
+        <h3><?php _e( 'Pages', 'sandbox' ) ?></h3>
+        <ul>
+          <?php wp_list_pages('title_li=&sort_column=menu_order' ) ?>
+        </ul>
+      </li>
 
-			<li id="categories">
-				<h3><?php _e( 'Categories', 'sandbox' ) ?></h3>
-				<ul>
-<?php wp_list_categories('title_li=&show_count=0&hierarchical=1') ?> 
+      <li id="categories">
+        <h3><?php _e( 'Categories', 'sandbox' ) ?></h3>
+        <ul>
+          <?php wp_list_categories('title_li=&show_count=0&hierarchical=1') ?>
+        </ul>
+      </li>
 
-				</ul>
-			</li>
+      <li id="archives">
+        <h3><?php _e( 'Archives', 'sandbox' ) ?></h3>
+        <ul>
+          <?php wp_get_archives('type=monthly') ?>
+        </ul>
+      </li>
+    <?php endif; // end primary sidebar widgets  ?>
+  </ul>
+</div><!-- #primary .sidebar -->
 
-			<li id="archives">
-				<h3><?php _e( 'Archives', 'sandbox' ) ?></h3>
-				<ul>
-<?php wp_get_archives('type=monthly') ?>
+<div id="secondary" class="sidebar">
+  <ul class="xoxo">
+    <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar(2) ) : // begin secondary sidebar widgets ?>
+      <li id="search">
+        <h3><label for="s"><?php _e( 'Search', 'sandbox' ) ?></label></h3>
+        <form id="searchform" class="blog-search" method="get" action="<?php bloginfo('home') ?>">
+          <div>
+            <input id="s" name="s" type="text" class="text" value="<?php the_search_query() ?>" size="10" tabindex="1" />
+            <input type="submit" class="button" value="<?php _e( 'Find', 'sandbox' ) ?>" tabindex="2" />
+          </div>
+        </form>
+      </li>
 
-				</ul>
-			</li>
-<?php endif; // end primary sidebar widgets  ?>
-		</ul>
-	</div><!-- #primary .sidebar -->
+      <?php wp_list_bookmarks('title_before=<h3>&title_after=</h3>&show_images=1') ?>
 
-	<div id="secondary" class="sidebar">
-		<ul class="xoxo">
-<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar(2) ) : // begin secondary sidebar widgets ?>
-			<li id="search">
-				<h3><label for="s"><?php _e( 'Search', 'sandbox' ) ?></label></h3>
-				<form id="searchform" class="blog-search" method="get" action="<?php bloginfo('home') ?>">
-					<div>
-						<input id="s" name="s" type="text" class="text" value="<?php the_search_query() ?>" size="10" tabindex="1" />
-						<input type="submit" class="button" value="<?php _e( 'Find', 'sandbox' ) ?>" tabindex="2" />
-					</div>
-				</form>
-			</li>
+      <li id="rss-links">
+        <h3><?php _e( 'RSS Feeds', 'sandbox' ) ?></h3>
+        <ul>
+          <li><a href="<?php bloginfo('rss2_url') ?>" title="<?php printf( __( '%s latest posts', 'sandbox' ), wp_specialchars( get_bloginfo('name'), 1 ) ) ?>" rel="alternate" type="application/rss+xml"><?php _e( 'All posts', 'sandbox' ) ?></a></li>
+          <li><a href="<?php bloginfo('comments_rss2_url') ?>" title="<?php printf( __( '%s latest comments', 'sandbox' ), wp_specialchars( get_bloginfo('name'), 1 ) ) ?>" rel="alternate" type="application/rss+xml"><?php _e( 'All comments', 'sandbox' ) ?></a></li>
+        </ul>
+      </li>
 
-<?php wp_list_bookmarks('title_before=<h3>&title_after=</h3>&show_images=1') ?>
+      <li id="meta">
+        <h3><?php _e( 'Meta', 'sandbox' ) ?></h3>
+        <ul>
+          <?php wp_register() ?>
 
-			<li id="rss-links">
-				<h3><?php _e( 'RSS Feeds', 'sandbox' ) ?></h3>
-				<ul>
-					<li><a href="<?php bloginfo('rss2_url') ?>" title="<?php printf( __( '%s latest posts', 'sandbox' ), wp_specialchars( get_bloginfo('name'), 1 ) ) ?>" rel="alternate" type="application/rss+xml"><?php _e( 'All posts', 'sandbox' ) ?></a></li>
-					<li><a href="<?php bloginfo('comments_rss2_url') ?>" title="<?php printf( __( '%s latest comments', 'sandbox' ), wp_specialchars( get_bloginfo('name'), 1 ) ) ?>" rel="alternate" type="application/rss+xml"><?php _e( 'All comments', 'sandbox' ) ?></a></li>
-				</ul>
-			</li>
+          <li><?php wp_loginout() ?></li>
+          <?php wp_meta() ?>
 
-			<li id="meta">
-				<h3><?php _e( 'Meta', 'sandbox' ) ?></h3>
-				<ul>
-					<?php wp_register() ?>
-
-					<li><?php wp_loginout() ?></li>
-					<?php wp_meta() ?>
-
-				</ul>
-			</li>
-<?php endif; // end secondary sidebar widgets  ?>
-		</ul>
-	</div><!-- #secondary .sidebar -->
+        </ul>
+      </li>
+    <?php endif; // end secondary sidebar widgets  ?>
+  </ul>
+</div><!-- #secondary .sidebar -->
