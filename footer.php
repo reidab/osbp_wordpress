5360334dc7d25c7e8c5c618b4e2b7f07556fa344
Kirsten Comandich <kcomandich@gmail.com>
2010-03-05 13:31:14 -0800

    "Open Source Bridge Foundation" => "Open Source Bridge"

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 	</div><!-- #container -->
 
 	<div id="footer">
-		&copy; 2008&ndash;<?php echo date('Y'); ?> Open Source Bridge Foundation | <a href='/contact-us'>Contact Us</a>
+		&copy; 2008&ndash;<?php echo date('Y'); ?> Open Source Bridge | <a href='/contact-us'>Contact Us</a>
 	</div><!-- #footer -->
 
 </div><!-- #wrapper .hfeed -->
