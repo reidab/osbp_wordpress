ac253e5c73e7438b2d15b3e74ecf952c53b73f19
Kirsten Comandich <kcomandich@gmail.com>
2010-12-08 10:16:53 -0800

    Added link to registration site in frontpage box.

--- frontpage.php
+++ frontpage.php
@@ -54,10 +54,12 @@ Template Name: Front Page
          </div>
          <div id="frontpage_content">
             <div id='frontpage-countdown'>
-               <h3>Open Source Bridge 2011: Registration now open!</h3>
+               <h3>Registration for 2011 is now open!</h3>
                <p>
                     A limited number of early-bird $175 tickets are available now!<br />
                     Want to help us plan the 2011 event? <a href='http://groups.google.com/group/osbridge' style='text-decoration: underline; color: #fff;'>Join our planning group!</a>
+
+                    <a href='http://osbridge.eventbrite.com/' class='button'>Register Now</a>
                </p>
             </div>
             <?php the_content(); ?>
